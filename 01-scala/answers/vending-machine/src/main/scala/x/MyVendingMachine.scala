8d5f30039fefc935eaca9d7712c980408b375e36
Mark Kerzner <mark@elephantscale.com>
2016-05-15 09:53:32 -0500

    tests added

--- 01-scala/answers/vending-machine/src/main/scala/x/MyVendingMachine.scala
+++ 01-scala/answers/vending-machine/src/main/scala/x/MyVendingMachine.scala
@@ -3,11 +3,14 @@ package x
 import scala.collection.mutable
 import scala.collection.mutable.{Map, HashMap}
 
+/**
+  * Trying to use val instead of var whenever possible
+  */
 class MyVendingMachine extends VendingMachine {
 
   val store: mutable.Map[String, Stock] = Map()
   var myStash = 0
-  
+
   override def addStockItem(item: String, price: Int, qty: Int): Int = {
     if (store.contains(item)) {
       // adding an item with a new price automatically re-prices existing stock
@@ -18,21 +21,23 @@ class MyVendingMachine extends VendingMachine {
       store.put(item, new Stock(item, price, qty))
       return qty
     }
-   }
+  }
 
   override def checkPrice(item: String): Int = {
-    if (store.contains(item)) {       
+    if (store.contains(item)) {
       store(item).getPrice()
     } else {
-      // 0 price on absent items
-      0
+      sys.error("checking price on item not in stock: " + item)
     }
   }
 
   override def buy(item: String): ReturnCode = {
     if (store.contains(item)) {
-      // TODO what happens when we are out of stock?
-      store.put(item, store(item).buy)
+      val myItem = store(item)
+      val trialNewBalance = myStash - 1 * myItem.getPrice()
+      if (trialNewBalance < 0) sys.error("Not enough money") else myStash = trialNewBalance
+      // TODO what happens when we are out of stock? 
+      store.put(item, myItem.buy)
       Success
     } else {
       ItemNotInStock
@@ -53,15 +58,19 @@ class Stock(item: String, price: Int, qty: Int) {
   def getItem(): String = {
     item
   }
+
   def getPrice(): Int = {
     price
   }
+
   def getQty: Int = {
     qty
   }
+
   def buy(): Stock = {
     new Stock(item, price, qty - 1)
   }
+
   def add(newPrice: Int, addQty: Int): Stock = {
     new Stock(item, newPrice, qty + addQty)
   }
--- 01-scala/answers/vending-machine/src/test/scala/x/MyVendingMachineTest.scala
+++ 01-scala/answers/vending-machine/src/test/scala/x/MyVendingMachineTest.scala
@@ -4,10 +4,35 @@ import org.specs2.mutable.Specification
 
 class MyVendingMachineTest extends Specification {
 
+
   "x.MyVendingMachineTest" should {
-    "correctly dispense items" in {
-      println("OK")
-      0.0 must be <= .1
+    "accept initial inventory" in {
+      new MyVendingMachine().addStockItem("candy", 20, 5) mustEqual 5
     }
+  }
+  "accept additinal inventory" in {
+    val machine = new MyVendingMachine
+    machine.addStockItem("candy", 20, 5)
+    machine.addStockItem("candy", 20, 2) mustEqual 7
+  }
+  "check item price" in {
+    val machine = new MyVendingMachine
+    machine.addStockItem("candy", 20, 5)
+    machine.checkPrice("candy") mustEqual 20
+  }
+  "accept deposit" in {
+    val machine = new MyVendingMachine
+    machine.deposit(100) mustEqual 100
+    machine.deposit(100) mustEqual 200
+    machine.deposit(0) mustEqual 200
+  }
+  
+  "dispence item" in {
+    val machine = new MyVendingMachine
+    machine.deposit(100)
+    machine.addStockItem("candy", 20, 5)
+    machine.buy("candy")
+    machine.balance mustEqual 80
+    machine.addStockItem("candy", 20, 5) mustEqual 9
   }
 }
