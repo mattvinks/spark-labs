3cd1be6753b0a5f8cc9e3ad12dced43561fa90cc
Tim Fox <tim@elephantscale.com>
2017-01-20 12:26:49 -0500

    Converted mtcars kmeans -> spark 2.1

--- 06-mllib/kmeans/kmeans_mtcars.scala
+++ 06-mllib/kmeans/kmeans_mtcars.scala
@@ -1,83 +1,33 @@
-/*
-   1 ) Enter these instructions on Spark Shell... one by one.
-        So you can see what's going on.
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
 
-   2) Also keep an eye on Spark Shell UI (port 4040) as you are executing the statements..
 
-    3) After you got it working, you can run the entire script as 
-            $   ~/spark/bin/spark-shell  -i  kmeans_mtcars.scala
-*/
-
-import org.apache.spark.mllib.clustering.KMeans
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.Vector
-
-// MT Cars Data is as follows:
-//"name","mpg","cyl","disp","hp","drat","wt","qsec","vs","am","gear","carb"
-//Header row does NOT appear in dataset
-def parseData(vals : RDD[String]) : RDD[(String, Vector)] = {
-  vals.map { s =>
-    val splitData = s.split(',')
-    val name = splitData(0)
-    val mpg = splitData(1)
-    val cyl = splitData(2)
-    // let's just consider mpg and cylinders for clustering
-    val doubles = Array(mpg,cyl).map(_.toDouble)
-    val vectors = Vectors.dense(doubles)
-    (name, vectors) // (key , value) RDD
-
-    // # If you want to consider all vectors, try this
-    //val numericFields = splitData.drop(1)
-    //val doubles = numericFields.map(_.toDouble)
-    // (name, doubles)
-  }
+// Conversion function for string --> float
+def convertColumn(df: org.apache.spark.sql.DataFrame, name:String, newType:String) = {
+  val df_1 = df.withColumnRenamed(name, "swap")
+  df_1.withColumn(name, df_1.col("swap").cast(newType)).drop("swap")
 }
 
 
-// Load and parse the data
-val data = sc.textFile("../../data/mtcars/mtcars.csv")
-data.foreach (println)
+// Loads data.
+val dataset = spark.read.option("header", "true").csv("../../data/mtcars/mtcars_header.csv")
 
-// parse the data and print
-val namesAndData = parseData(data)
-namesAndData.foreach(println)
+val dataset1 = convertColumn(dataset, "mpg", "float")
+val dataset2 = convertColumn(dataset1, "cyl", "float")
 
+val assembler = new VectorAssembler().setInputCols(Array("mpg", "cyl")).setOutputCol("features")
 
-// Get only the vector out of mpg and cyl
-val onlyVectors = namesAndData.map { case (name, vector) => vector } 
-//one-liner: 
-//      val onlyVectors = data.map(s => Vectors.dense(s.split(',').drop(1).map(_.toDouble)))
-onlyVectors.foreach(println)
+val featureVector = assembler.transform(dataset2)
 
+// Trains a k-means model.
+val kmeans = new KMeans().setK(2).setSeed(1L)
+val model = kmeans.fit(featureVector)
 
-// ## TODO-last : Enable caching as a last step
-// ##  Since we are doing multiple iterations, may be caching will help
-// onlyVectors.cache
-// onlyVectors.count  // force caching
+// Evaluate clustering by computing Within Set Sum of Squared Errors.
+val WSSSE = model.computeCost(featureVector)
+println(s"Within Set Sum of Squared Errors = $WSSSE")
 
-
-//  Run Kmeans
-//  Kmeans (vectors,   number_of_clusters,  num_iterations)
-//      number_of_clusters : (2)  this is the 'K' in K-Means.  Start with 2
-//      number_if_iterations : (20) start with 10 , and try 20
-val clusters = KMeans.train(onlyVectors, 2, 10)
-
-// Evaluate clustering by computing Within Set Sum of Squared Errors
-val WSSSE = clusters.computeCost(onlyVectors)
-println("Within Set Sum of Squared Errors = " + WSSSE)
-// we will plot WSSSE vs k  and choose 'elbow'
-
-// Print out a list of the clusters and each point of the clusters
-// val groupedClusters = namesAndData.groupBy{rdd => clusters.predict(rdd._2)}
-// groupedClusters.collect
-
-// pretty print
-val carsByCluster =namesAndData.map(s => (clusters.predict(s._2), (s._1,s._2))).sortByKey().collect()
-carsByCluster.foreach { println }
-
-// TODO - evaluate : How are the cars clustering?
-// change the value of 'k' in line 61 and try it again
-
-
-exit
+// Shows the result.
+println("Cluster Centers: ")
+model.clusterCenters.foreach(println)
