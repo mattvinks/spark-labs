c7d4adf96f250d26ae53b2abdad36309fcc8b231
Sujee Maniyam <SUJEE@SUJEE.NET>
2017-11-06 22:12:29 -0700

    big python update and refactor [4.00]

---
+++ package-labs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## install nbconvert as
+##      conda install nbconvert
+##              or
+##      pip install nbconvert
+
+## cleanup html
+find . -name "*.html" -print0 | xargs -0 rm -f
+
+## convert ipynb notebooks into HTML
+notebooks=$(find . -type f -name "*.ipynb" | grep -v ".ipynb_checkpoints" )
+
+jupyter nbconvert ${notebooks}
+
+# move files in right place
+mv -f private/README-python1.html  .
+
+## Change all the links from README.html
+sed 's/ipynb/html/g' < Spark-python.html > a.html
+mv -f a.html  Spark-python.html
+
+# create a zipfile
+
+#zip_file_name=$(basename `pwd`)
+#rm -f ../$zip_file_name.zip
+#(cd .. ; zip -r ${zip_file_name}.zip ${zip_file_name} -x *.git*   -x *metastore_db*  -x *derby.log*  -x *.out  -x *.zip  -x *.ipynb_checkpoints*  ;  mv ${zip_file_name}.zip ${zip_file_name}  )
+
+rm -f *.zip
+git archive --format=zip HEAD -o spark-labs.zip
