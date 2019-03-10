88d5ad2e52da2f978031c5e6fd590c75d4f3a6c8
Sujee Maniyam <sujee@sujee.net>
2019-03-09 19:03:48 -0800

    run jupyter script

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## if you need to set anaconda path explicitly, do so here
+## how ever, if you are using 'custom python environment', 
+## leave this line commented out (do not over-ride the default path)
+
 #export PATH=$HOME/anaconda3/bin:$PATH
 export SPARK_HOME=$HOME/spark
 jupyter lab
